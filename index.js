89039e165cc1c29d2b147f30d7413408773fbb63
Drummer Dee <drummer.deja@gmail.com>
2022-06-10 15:42:14 -0400

    syntax error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const charsName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://power-puff.herokuapp.com//api/${charsName}`)
+        const response = await fetch(`https://power-puff.herokuapp.com/api/${charsName}`)
         const data = await response.json()
         console.log(data)
         document.querySelector('h2').innerText = data.animeName
