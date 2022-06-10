8217b23eb14005dde4b2b161ce3fb6642139bca0
Drummer Dee <drummer.deja@gmail.com>
2022-06-10 08:14:46 -0400

    test

---
+++ public/index.js
@@ -0,0 +1,17 @@
+// document.querySelector('button').addEventListener('click', apiRequest)
+
+// async function apiRequest(){
+//     const charsName = document.querySelector('input').value
+//     try{
+//         const response = await fetch(`https://power-puff.herokuapp.com/api/${charsName}`)
+//         const data = await response.json()
+//         console.log(data)
+//         document.querySelector('h2').innerText = data.animeName
+//     }
+//     catch(error){
+//         console.log('Something\'s wrong');
+//     }
+// }
+
+
+
