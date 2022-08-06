ca6726d360f6af8e61d93b2c39c142ba9bd8220e
Drummer Dee <drummer.deja@gmail.com>
2022-08-05 21:27:57 -0400

    splitting back/front end

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+document.querySelector('button').addEventListener('click', apiRequest);
+async function apiRequest() {
+    const charsName = document.querySelector('input').value
+    try {
+        const response = await fetch(`https://power-puff.herokuapp.com/api/${charsName}`)
+        const data = await response.json()
+        console.log(data)
+    }
+}
