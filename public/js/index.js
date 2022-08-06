a06dd72eebff0f7aa39aa745456a8168e8ad7b33
Danniel Rolfe <talkto@dannielrolfe.com>
2022-08-06 10:40:47 -0700

    Refactor dom update function

--- public/js/index.js
+++ public/js/index.js
@@ -21,9 +21,11 @@
     // Functions
    function apiRequest() {
         let searchQueryValue = searchQuery.value;
-        // if (!inputValue.length) {
-        //     alert('PLEASE INPUT VALUE');
-        // }
+
+        if (!searchQueryValue.length) {
+            // TODO implement something visually nicer
+            alert('Please input value');
+        }
 
        if (searchQueryValue.length) { // either 0 or greater; if 0 = false if +1 true
             console.log('API Request');
@@ -34,23 +36,6 @@
                     console.error('ERROR: ', err);
                 });
         }
-
-       // try {
-       //      responseToJson = response.json();
-       //
-       //      if (!response) {
-       //          console.error('No data to show');
-       //      }
-       //
-       //      if (response) {
-       //          console.log('Expect data to be in a json format: ', responseToJson);
-       //          updateDOMList(responseToJson);
-       //      }
-       //  } catch (err) {
-       //      console.err('API Request failure: ', err)
-       //  }
-
-        //return responseToJson;
     };
 
     function updateDOMList(data) {
@@ -59,52 +44,47 @@
             return; // exit out of the function
         }
         if (data) {
-            document.getElementById('charName').innerText = `Name : ${data.name}`; //grabs name from obj and inserts it into DOM
-            document.getElementById('charAnime').innerText = `Anime Name: ${data.animeName}`;
-            document.getElementById('charAcces').innerText = `Accessories: ${data.accessories}`;
-            document.getElementById('HairColor').innerText = "Hair Color:";
-            document.getElementById('charHair').innerText = data.hairColor;
-            document.getElementById('SuperPowers').innerText = "Super Powers:";
-            document.getElementById('charSuper').innerText = data.superPowers;
-            document.getElementById('ArchNemesis').innerText = "Arch Nemesis:";
-            document.getElementById('charNemesis').innerText = data.archNemesis;
-            document.getElementById('Phrase').innerText = "Phrase:";
-            document.getElementById('charPhrase').innerText = data.phraseSaid;
-            document.getElementById('VoiceActor').innerText = "Voice Actor:";
-            document.getElementById('charVoice').innerText = data.voicedBy;
+            const elements = [
+                {
+                    id: 'charName',
+                    label: 'Name',
+                    value: data.name
+                },
+                {
+                    id: 'charAnime',
+                    label: 'Anime Name',
+                    value: data.animeName
+                },
+                {
+                    id: 'charAcces',
+                    label: 'Accessories',
+                    value: data.accessories
+                },
+                ///....
+            ];
+
+            elements.forEach(value => {
+                if (value.id, value.label, value.value) {
+                    document.getElementById(value.id).innerText = `${value.label}: ${value.value}`;
+                }
+            });
+
+
+
+
+            // document.getElementById('charName').innerText = `Name : ${data.name}`; //grabs name from obj and inserts it into DOM
+            // document.getElementById('charAnime').innerText = `Anime Name: ${data.animeName}`;
+            // document.getElementById('charAcces').innerText = `Accessories: ${data.accessories}`;
+            // document.getElementById('HairColor').innerText = "Hair Color:";
+            // document.getElementById('charHair').innerText = data.hairColor;
+            // document.getElementById('SuperPowers').innerText = "Super Powers:";
+            // document.getElementById('charSuper').innerText = data.superPowers;
+            // document.getElementById('ArchNemesis').innerText = "Arch Nemesis:";
+            // document.getElementById('charNemesis').innerText = data.archNemesis;
+            // document.getElementById('Phrase').innerText = "Phrase:";
+            // document.getElementById('charPhrase').innerText = data.phraseSaid;
+            // document.getElementById('VoiceActor').innerText = "Voice Actor:";
+            // document.getElementById('charVoice').innerText = data.voicedBy;
         }
     }
 })();
-
-
-
-
-// async function apiRequest() {
-//     const charsName = document.querySelector('input').value
-//     try {
-//         const response = await fetch(`https://power-puff.herokuapp.com/api/${charsName}`)
-//         const data = await response.json()
-//         console.log(data)
-// function doc(param){
-//    return document.getElementById(param).innertext
-// }
-// doc('charName') = `Name: ${data.name}`
-//
-//         document.getElementById('charName').innerText = `Name : ${data.name}` //grabs name from obj and inserts it into DOM
-//         document.getElementById('charAnime').innerText = `Anime Name: ${data.animeName}`
-//         document.getElementById('charAcces').innerText = `Accessories: ${data.accessories}`
-//         document.getElementById('HairColor').innerText = "Hair Color:"
-//         document.getElementById('charHair').innerText = data.hairColor
-//         document.getElementById('SuperPowers').innerText = "Super Powers:"
-//         document.getElementById('charSuper').innerText = data.superPowers
-//         document.getElementById('ArchNemesis').innerText = "Arch Nemesis:"
-//         document.getElementById('charNemesis').innerText = data.archNemesis
-//         document.getElementById('Phrase').innerText = "Phrase:"
-//         document.getElementById('charPhrase').innerText = data.phraseSaid
-//         document.getElementById('VoiceActor').innerText = "Voice Actor:"
-//         document.getElementById('charVoice').innerText = data.voicedBy
-//     }
-//     catch (error) {
-//         console.log(error);
-//     };
-
