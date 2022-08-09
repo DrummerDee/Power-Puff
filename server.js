e35779d35ffa87aa592b95511b66713242c1bfd1
Drummer Dee <drummer.deja@gmail.com>
2022-08-09 10:00:22 -0400

    change

--- server.js
+++ server.js
@@ -11,50 +11,50 @@ app.get('/', (request, response) => {
 })
 
 //empty array for my data to be stored
-const char = {
-  'blossom': {
-    "name": "Blossom",
-    "animeName": "Momoko Akatsumi",
-    "accessories": "Red Bow",
-    "hairColor": "Orange",
-    "superPowers": "Flight, Super Strength, Ice Breath",
-    "archNemesis": "Mojo Jojo, Ruffle ToughEm Boys, Him",
-    "phraseSaid": "I guess we shouldn't judge people by what they look like",
-    "voicedBy": "Cathy Cavadini"
-  },
-  'buttercup': {
-    "name": "Buttercup",
-    "animeName": "Kaoru Matsubara",
-    "accessories": "She has none",
-    "hairColor": "Black",
-    "superPowers": "Flight, Super Strength, FireBalls",
-    "archNemesis": "Mojo Jojo, Ruffle ToughEm Boys, Him",
-    "phraseSaid": "Yeah, but it's a skill that you cant do. And you cant do",
-    "voicedBy": "E.G. Daily"
-  },
-  'bubbles': {
-    "name" : "Bubbles",
-    "animeName": "Miyako Gotokuji",
-    "accessories": "Purple Octopus",
-    "hairColor": "Blonde",
-    "superPowers": "Flight, Super Strength, Multilingual",
-    "archNemesis": "Mojo Jojo, Ruffle ToughEm Boys, Him",
-    "phraseSaid": "I'm going to be the prettiest girl at the party",
-    "voicedBy": "Tara Strong"
-  }
-};
+// const char = {
+//   'blossom': {
+//     "name": "Blossom",
+//     "animeName": "Momoko Akatsumi",
+//     "accessories": "Red Bow",
+//     "hairColor": "Orange",
+//     "superPowers": "Flight, Super Strength, Ice Breath",
+//     "archNemesis": "Mojo Jojo, Ruffle ToughEm Boys, Him",
+//     "phraseSaid": "I guess we shouldn't judge people by what they look like",
+//     "voicedBy": "Cathy Cavadini"
+//   },
+//   'buttercup': {
+//     "name": "Buttercup",
+//     "animeName": "Kaoru Matsubara",
+//     "accessories": "She has none",
+//     "hairColor": "Black",
+//     "superPowers": "Flight, Super Strength, FireBalls",
+//     "archNemesis": "Mojo Jojo, Ruffle ToughEm Boys, Him",
+//     "phraseSaid": "Yeah, but it's a skill that you cant do. And you cant do",
+//     "voicedBy": "E.G. Daily"
+//   },
+//   'bubbles': {
+//     "name" : "Bubbles",
+//     "animeName": "Miyako Gotokuji",
+//     "accessories": "Purple Octopus",
+//     "hairColor": "Blonde",
+//     "superPowers": "Flight, Super Strength, Multilingual",
+//     "archNemesis": "Mojo Jojo, Ruffle ToughEm Boys, Him",
+//     "phraseSaid": "I'm going to be the prettiest girl at the party",
+//     "voicedBy": "Tara Strong"
+//   }
+// };
 
 
 
-app.get('/api/:charName', (request, response) => {
-    const charsName = request.params.charName.toLowerCase()
-    if(char[charsName]){ 
-      console.log(char[charsName])
-        response.json(char[charsName])
-    } else {
-       response.json(['unknown'])
-   }
-})
+// app.get('/api/:charName', (request, response) => {
+//     const charsName = request.params.charName.toLowerCase()
+//     if(char[charsName]){ 
+//       console.log(char[charsName])
+//         response.json(char[charsName])
+//     } else {
+//        response.json(['unknown'])
+//    }
+// })
 
 
 //way for server to be connected
