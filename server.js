83d9095bd54affbcffd430036a592d93e4f1d347
Drummer Dee <drummer.deja@gmail.com>
2022-08-09 20:06:46 -0400

    added more characters

--- server.js
+++ server.js
@@ -9,74 +9,106 @@ app.use(cors());
 
 // serves up html page 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html')
 });
-
-//empty array for my data to be stored
-//'blossom': {
-  //     "name": "Blossom",
-  //     "animeName": "Momoko Akatsumi",
-  //     "accessories": "Red Bow",
-  //     "hairColor": "Orange",
-  //     "superPowers": "Flight, Super Strength, Ice Breath",
-  //     "archNemesis": "Mojo Jojo, Ruffle ToughEm Boys, Him",
-  //     "phraseSaid": "I guess we shouldn't judge people by what they look like",
-  //     "voicedBy": "Cathy Cavadini"
-//   'buttercup': {
-//     "name": "Buttercup",
-//     "animeName": "Kaoru Matsubara",
-//     "accessories": "She has none",
-//     "hairColor": "Black",
-//     "superPowers": "Flight, Super Strength, FireBalls",
-//     "archNemesis": "Mojo Jojo, Ruffle ToughEm Boys, Him",
-//     "phraseSaid": "Yeah, but it's a skill that you cant do. And you cant do",
-//     "voicedBy": "E.G. Daily"
-//   },
-//   'bubbles': {
-//     "name" : "Bubbles",
-//     "animeName": "Miyako Gotokuji",
-//     "accessories": "Purple Octopus",
-//     "hairColor": "Blonde",
-//     "superPowers": "Flight, Super Strength, Multilingual",
-//     "archNemesis": "Mojo Jojo, Ruffle ToughEm Boys, Him",
-//     "phraseSaid": "I'm going to be the prettiest girl at the party",
-//     "voicedBy": "Tara Strong"
-//   }
-// };
 
 // class constuctor to input data more efficiently 
 class Char {
-  constructor (name,animeName,accessories,hairColor,superPowers,archNemesis,phraseSaid,voicedBy){
-  this.name = name;
-  this.animeName = animeName;
-  this.accessories = accessories;
-  this.hairColor = hairColor;
-  this.superPowers = superPowers;
-  this.archNemesis = archNemesis;
-  this.phraseSaid = phraseSaid;
-  this.voicedBy = voicedBy;
-}
+  constructor(name, accessories, hairColor, superPowers, archNemesis, phraseSaid, voicedBy) {
+    this.name = name;
+    this.accessories = accessories;
+    this.hairColor = hairColor;
+    this.superPowers = superPowers;
+    this.archNemesis = archNemesis;
+    this.phraseSaid = phraseSaid;
+    this.voicedBy = voicedBy;
+  }
 }
 // initializing new instances of each character
-const buttercup = new Char('Buttercup','Kaoru Matsubara','She has none','Black','Flight,Super Strength,FireBalls','Mojo Jojo, Ruffle ToughEm Boys, Him',`Yeah, but it's a skill that you can't do`,'E.G Daily'),
-      bubbles = new Char('Bubbles','Miyako Gotokuji','Purple Octopus','Blonde','Flight,Super Strength,Multilingual','Mojo Jojo,Ruffle ToughEm Boys,Him',`I'm going to be the prettiest girl at the party`,'Tara Strong'),
-      blossom = new Char('Blossom','Momoko Akatsumi','Red Bow','Orange','Flight,Super Strength, Ice Breath',`I guess we shouldn't judge people by what they look like`,'Cathy Cavadini'),
-      // storing new objects in chars object
-      chars = {buttercup,bubbles,blossom}
+const buttercup = new Char('Buttercup', 'She has none', 'Black', 'Flight,Super Strength,FireBalls', 'Mojo Jojo, Ruffle ToughEm Boys, Him', `Yeah, but it's a skill that you can't do`, 'E.G Daily'),
+  bubbles = new Char('Bubbles', 'Purple Octopus', 'Blonde', 'Flight,Super Strength,Multilingual', 'Mojo Jojo,Ruffle ToughEm Boys,Him', `I'm going to be the prettiest girl at the party`, 'Tara Strong'),
+  blossom = new Char('Blossom', 'Red Bow', 'Orange', 'Flight,Super Strength, Ice Breath', `I guess we shouldn't judge people by what they look like`, 'Cathy Cavadini'),
+  professor = new Char('Professor Utonium', 'Black Pipe', 'Black', 'None', 'Girls!', 'Mojo Jojo', 'Tom Kane'),
+  mayor = new Char('Mayor/Barney', 'Monocole & Small top hat', 'None', 'None', '', 'He has none technically', 'Tom Kenny, Jim Cunnings'),
+  msbellum = new Char('Miss Sara Bellum', 'The mayor', 'Red'),
+  mskeane = new Char('Miss Keane'),
+  narrator = new Char('Narrator'),
+  talkingdog = new Char('Talking Dog'),
+  mitchmitchelson = new Char('Mitch Mitchelson'),
+  thederbytantes = new Char('The Derbytantes'),
+  donny = new Char('Donny'),
+  mojojojo = new Char('Mojo Jojo'),
+  him = new Char('HIM'),
+  fuzzylumpkins = new Char('Fuzzy Lumpkins'),
+  princessmorbucks = new Char('Princess Morbucks'),
+  ace = new Char('Ace - Ganggreen Member'),
+  snake = new Char('Snake - Ganggreen Member'),
+  lilarturo = new Char('Lil Arturo'),
+  grubber = new Char('Grubber'),
+  bigbilly = new Char('Big Billy'),
+  bossman = new Char('Bossman'),
+  junior = new Char('junior'),
+  slim = new Char('Slim - an Aneoba boy'),
+  sedusa = new Char('Sedusa'),
+  brick = new Char('Brick'),
+  boomer = new Char('Boomer'),
+  butch = new Char('butch'),
+  packrat = new Char('packrat'),
+  allegro = new Char('Allegro'),
+  manboy = new Char('Manboy'),
+  silico = new Char('Silico'),
+  unknown = 'unknown',
+
+  // storing new objects in chars object
+  chars = {
+    buttercup,
+    bubbles,
+    blossom,
+    professor,
+    mayor,
+    msbellum,
+    mskeane,
+    narrator,
+    talkingdog,
+    mitchmitchelson,
+    thederbytantes,
+    donny,
+    mojojojo,
+    him,
+    fuzzylumpkins,
+    princessmorbucks,
+    ace,
+    snake,
+    lilarturo,
+    grubber,
+    bigbilly,
+    bossman,
+    junior,
+    slim,
+    sedusa,
+    brick,
+    boomer,
+    butch,
+    packrat,
+    allegro,
+    manboy,
+    silico, 
+    unknown
+  }
 
 // will return whatever name is put after the api/
-app.get('/api/:name', (req,res) => {
-// put the param targeting the name attribute in a variable and making it case insensitive
+app.get('/api/:name', (req, res) => {
+  // put the param targeting the name attribute in a variable and making it case insensitive
   const charName = req.params.name.toLowerCase()
-  if(chars[charName]){ 
+  if (chars[charName]) {
     console.log(chars[charName])
-        res.json(chars[charName])
-    } else {
-        res.json('unknown to this universe')
-       }
+    res.json(chars[charName])
+  } else {
+    res.json('unknown to this universe')
+  }
 });
 // to retrieve general api data 
-app.get('/characters', (req,res) => {
+app.get('/characters', (req, res) => {
   res.json(chars)
 });
 //tells me that the port has successfully connected
